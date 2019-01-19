8867958ece11d76d920fc30875fb67716ddc367d
rso16 <rso16>
2019-01-19 21:58:57 +0100

    MysqlHanlder documented

--- src/MysqlHandler/MysqlHandler.cpp
+++ src/MysqlHandler/MysqlHandler.cpp
@@ -1,15 +1,19 @@
 // Include Header Files
 #include "MysqlHandler.h"
 
-
+/**
+* @brief connects to the db-server.
+* @returns 1 if succesfull, -1 if the initialization failed & -2 if connection failed.
+* @author Rick Overhorst
+*/
 int MysqlHandler::connect(char *server, char *user, char *pass, char *db_name)
 {
 	connection = mysql_init(NULL);
 
 	if (!connection)
 	{
-	printf("Mysql Initialization Failed");
-	return 1;
+		printf("Mysql Initialization Failed");
+		return -1;
 	}
 
 	connection = mysql_real_connect(connection, server, user, pass, db_name, 0,NULL,0);
@@ -22,10 +26,15 @@ int MysqlHandler::connect(char *server, char *user, char *pass, char *db_name)
 	else
 	{
 		printf("Connection Failed\n");
-		return -1;
+		return -2;
 	}
 }
 
+/**
+* @brief Executes the SQL query.
+* @returns The Results of the query.
+* @author Rick Overhorst
+*/
 MYSQL_RES MysqlHandler::executeSQL(char* sql)
 {
 	MYSQL_RES *res_set;
@@ -42,6 +51,12 @@ MYSQL_RES MysqlHandler::executeSQL(char* sql)
 	return *res_set;
 }
 
+/**
+* @brief Prints the result of the query
+* @returns 1
+* @warning Error checking not yet added!
+* @author Rick Overhorst
+*/
 int MysqlHandler::printMysqlRes(MYSQL_RES *res_set)
 {
 	unsigned int numrows = mysql_num_rows(res_set);
@@ -58,6 +73,12 @@ int MysqlHandler::printMysqlRes(MYSQL_RES *res_set)
 	return 1;
 }
 
+/**
+* @brief closes the connection with the db-server
+* @returns 1
+* @warning Error checking not yet added!
+* @author Rick Overhorst
+*/
 int MysqlHandler::close()
 {
 	mysql_close(connection);
